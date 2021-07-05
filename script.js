b46e8bafcd05dde10e26dd2cea91f01cb1744cda
fajartd02 <fajarmh2@gmail.com>
2021-07-05 23:39:05 +0700

    Revision

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ $(document).ready( () => {
             $('.nav').removeClass('stick');
         }
     });
+
+    var scroll_link = $('.point');
+    scroll_link.click(function(e){
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    });
 });
